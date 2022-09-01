c8114c8aa0b92fff1268d36941dd4ed62fb37105
Ника Воронина <nika_voronina_95@mail.ru>
2022-09-01 19:45:42 +0300

    Task_2 added

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -4,3 +4,14 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.Write("Введите значение M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение N: ");
+int N = int.Parse(Console.ReadLine()!);
+int sum = 0;
+for (int i = M; i <= N; i++)
+{   if (i % 1 == 0) 
+    sum = sum + i;    
+}
+Console.WriteLine($"Сумма натуральных элементов от M до N = {sum}");
