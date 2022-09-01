8d3dc1101c7cd8341020ae584c88c8f5c196e748
Ника Воронина <nika_voronina_95@mail.ru>
2022-09-01 19:54:23 +0300

    Task_1 added

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -11,8 +11,16 @@ int M = int.Parse(Console.ReadLine()!);
 Console.Write("Введите значение N: ");
 int N = int.Parse(Console.ReadLine()!);
 if (M > N)
-for (int i = N; i <= M; i++)
-    Console.Write($" {i}");
-else
-    for (int i = M; i <= N; i++)
+{
+    for (int i = N; i <= M; i++)
+    {
         Console.Write($" {i}");
+    }
+}
+else
+{
+    for (int i = M; i <= N; i++)
+    {
+        Console.Write($" {i}");
+    }
+}
