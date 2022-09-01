fe210fa2be09f45dc86da1d87bd9c4bcf6efe8f2
Ника Воронина <nika_voronina_95@mail.ru>
2022-09-01 19:36:19 +0300

    Task_1 added

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -5,3 +5,14 @@
 // M = 1; N = 5. -> ""1, 2, 3, 4, 5""
 
 // M = 4; N = 8. -> ""4, 6, 7, 8""
+
+Console.Write("Введите значение M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение N: ");
+int N = int.Parse(Console.ReadLine()!);
+if (M > N)
+for (int i = N; i <= M; i++)
+    Console.Write($" {i}");
+else
+    for (int i = M; i <= N; i++)
+        Console.Write($" {i}");
