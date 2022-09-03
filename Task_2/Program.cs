899a6f68604d36ea5264ca68584a1947ea0d2bff
Ника Воронина <nika_voronina_95@mail.ru>
2022-09-03 18:46:40 +0300

    Task_2 added

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -5,16 +5,26 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-Console.Write("Введите значение M: ");
-int M = int.Parse(Console.ReadLine()!);
-Console.Write("Введите значение N: ");
-int N = int.Parse(Console.ReadLine()!);
-int sum = 0;
-for (int i = M; i <= N; i++)
-{   
-    if (i % 1 == 0)
+void PrintSum(int m, int n, int sum)
+{
+    sum = sum + n;
+    if (n <= m)
     {
-        sum = sum + i;
-    }    
+        Console.Write($"Сумма натуральных элементов = {sum}");
+        return;
+    }
+    PrintSum(m, n - 1, sum);
+
 }
-Console.WriteLine($"Сумма натуральных элементов от M до N = {sum}");
+Console.Write("Введите M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine()!);
+int temp = m;
+
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+PrintSum(m, n, temp = 0);
